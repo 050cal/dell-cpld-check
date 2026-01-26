a444f4855cad13bfad5f90536dcbcb09cc4e5c38
Calvin Brooks <123132758+050cal@users.noreply.github.com>
2026-01-26 09:55:31 -0800

    Update scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -14,7 +14,7 @@ CPLD_PAGES = {
     "R650":   "https://www.dell.com/support/home/en-us/drivers/driversdetails?driverid=fctdf",
     "R750":   "https://www.dell.com/support/home/en-us/drivers/driversdetails?driverid=fctdf",
     "C6525":  "https://www.dell.com/support/home/en-us/drivers/driversdetails?driverid=wv5d3",
-    "T340":  "https://www.dell.com/support/home/en-us/drivers/driversdetails?driverid=r5kd4",
+    "PowerEdge T340":  "https://www.dell.com/support/home/en-us/drivers/driversdetails?driverid=r5kd4",
     "R450":  "https://www.dell.com/support/home/en-us/drivers/driversdetails?driverid=pryjm",
     "R350":  "https://www.dell.com/support/home/en-us/drivers/driversdetails?driverid=w1htn",
 }
